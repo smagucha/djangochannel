252db172be337f379e4e75981d5375233dbb56ba
smagucha <smagucha@gmail.com>
2021-04-14 13:23:45 +0300

    webso

--- webchannel/webchannel/settings.py
+++ webchannel/webchannel/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 # Channels
 ASGI_APPLICATION = 'webchannel.asgi.application'
@@ -145,7 +145,7 @@ CHANNEL_LAYERS = {
 
 ASGI_APPLICATION = 'webchannel.asgi.application'
 
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static')
 ]
